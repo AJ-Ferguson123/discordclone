f9c4bb8c10cdf3eaf30461b68de9fc54d4666d1c
AJ-Ferguson123 <50592799+AJ-Ferguson123@users.noreply.github.com>
2021-11-16 08:22:42 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Clone Building Time</h2>
+    </div>
+  );
+}
+
+export default App;
